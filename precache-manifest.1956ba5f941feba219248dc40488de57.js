c4899a6211241c5ace1e4dc7779bd8a353c33fe9
webNie <3048615550@qq.com>
2019-11-14 12:09:40 +0800

    我自己设计最美的网站

---
+++ precache-manifest.1956ba5f941feba219248dc40488de57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d475ea9688dba41f32b538d19ec8386",
+    "url": "./index.html"
+  },
+  {
+    "revision": "787b796201567d416dec",
+    "url": "./static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "607a06885ab0220eb8a6",
+    "url": "./static/css/main.1592d403.chunk.css"
+  },
+  {
+    "revision": "787b796201567d416dec",
+    "url": "./static/js/2.f100628d.chunk.js"
+  },
+  {
+    "revision": "607a06885ab0220eb8a6",
+    "url": "./static/js/main.b1579fb0.chunk.js"
+  },
+  {
+    "revision": "6b77e587a8e6b551ce12",
+    "url": "./static/js/runtime-main.7886cc3c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9109374673c881cc33e9a78d9d8bc695.js"
+  "./precache-manifest.1956ba5f941feba219248dc40488de57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
